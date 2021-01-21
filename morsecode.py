92a232fffb2498dd2af47250b9ec109ca1171c4e
anawkward <food1121@naver.com>
2021-01-21 19:27:34 +0900

    update my code

--- morsecode.py
+++ morsecode.py
@@ -174,7 +174,7 @@ def encoding_sentence(english_sentence):
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
     english_sentence = " ".join(english_sentence.strip().split())
-    result = ' '.join(map(encoding_character, english_sentence.strip()))
+    result = ' '.join([x for x in map(encoding_character, english_sentence.strip()) if x != ''])
 
     return result
     # ==================================
