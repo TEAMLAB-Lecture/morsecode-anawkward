73ccb528171c5b387b295df1b8486497a483f3b6
anawkward <food1121@naver.com>
2021-01-21 17:39:57 +0900

    update my code

--- morsecode.py
+++ morsecode.py
@@ -104,7 +104,7 @@ def get_cleaned_english_sentence(raw_english_sentence):
     """
     # ===Modify codes below=============
     # 조건에 따라 변환되어야 할 결과를 result 변수에 할당 또는 필요에 따라 자유로운 수정
-    result = raw_english_sentence.replace('.').replace(',').replace('!').replace('?')
+    result = raw_english_sentence.replace('.', "").replace(',', "").replace('!', "").replace('?', "")
 
     return result
     # ==================================
